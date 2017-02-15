ac6faa6b65dba0fa2944eacaace4dfc2c5c42128
Matthew Prasinov <matthewjumpsoffbuildings@gmail.com>
2017-02-16 09:28:19 +1030

    fixed jquery global

--- source/app.js
+++ source/app.js
@@ -1,17 +1,22 @@
 //
 require('./scss/main.scss')
 
+
+global.jQuery = require('jquery')
 require('gsap')
+const $ = global.jQuery
 
 const m = require('./js/module')
-const $ = require('jquery')
 const Barba = require('barba.js')
+
+
+const flexslider = require('flexslider')
 
 
 $(document).ready(() => {
 	console.log("jquery works")
 
-	console.log(Barba)
+	$('.flexslider').flexslider()
 
 	TweenLite.to("h1", 2.5, {color:"#FF0000", fontSize:"10vw", ease: Expo.easeInOut})
 })
